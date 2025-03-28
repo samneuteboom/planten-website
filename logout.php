2c5a65a80d62ffc07c5a23b5c120026b13744e83
samneuteboom <6040120@mborijnland.nl>
2025-03-28 11:51:32 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,12 @@
 <?php
+//---------------------------//
+// Naam script     : logout.php
+// Omschrijving     : deze pagina is er voor dat je kan uitloggen wanneer je bent ingelogd op een acount
+// Naam ontwikkelaar : sam
+//---------------------------//
+?>
+
+<?php
 session_start();
 session_destroy();
 header("Location: index.html");
